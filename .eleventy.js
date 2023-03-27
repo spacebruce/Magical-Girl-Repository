909999fb21f9fec252c0b74dc4ce7cde902df040
spacebruce <noel.abercrombie@hotmail.com>
2023-03-27 15:05:30 +0100

    only show years on timeline

--- .eleventy.js
+++ .eleventy.js
@@ -14,17 +14,15 @@ module.exports = function (eleventyConfig)
   const {
       DateTime
     } = require("luxon");
-  
-      eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {
-          zone: 'utc'
-        }).toFormat('yyyy-MM-dd');
-      });
-  
-      eleventyConfig.addFilter("readableDate", dateObj => {
+
+    eleventyConfig.addFilter("justYear", (dateString) => {
+      dateObj = new Date(dateString);
+      return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy');
+    });
+    eleventyConfig.addFilter("readableDate", dateObj => {
       return DateTime.fromJSDate(dateObj, {
         zone: 'utc'
-      }).toFormat("dd-MM-yyyy");
+      }).toFormat("dd-mm-yyyy");
     });
 
     eleventyConfig.addCollection("tagGroup", function (collectionApi) {
--- posts/minky-momo.md
+++ posts/minky-momo.md
@@ -12,6 +12,6 @@ date: 1982-03-18
 <table>
 <tr><th>Image</th><th>Episode</th><th>Date</th><th>Title</th><th>Japanese</th><th>Notes</th>
 {%- for post in collections.minky-momo-episodes -%}
-  <tr><th><img src="{{ post.url | url }}{{ post.data.image }}"></th><th><a href="{{ post.url | url }}">{{ post.data.episode }}</a></th><th>{{ post.date | htmlDateString }}</th><th>{{ post.data.title }}</th><th>{{ post.data.name }}</th></tr></a>
+  <tr><th><img src="{{ post.url | url }}{{ post.data.image }}"></th><th><a href="{{ post.url | url }}">{{ post.data.episode }}</a></th><th>{{ post.date | readableDate }}</th><th>{{ post.data.title }}</th><th>{{ post.data.name }}</th></tr></a>
 {%- endfor -%}
 </table>
